70fdeb75b4281f3a4355af358c1ec2e85ba4fdcb
B0nifaci0 <al221411699@gmail.com>
2022-06-24 08:08:20 -0500

    fisrt comit

---
+++ asset/javascript/layout.js
@@ -0,0 +1,5 @@
+/* para ejecutar jquery o javascript en html es con la etiqueta scrip */
+    $(document).ready(function(){
+      $('#menuContainer').load('./layouts/menu.html');
+      $('#footerContainer').load('./layouts/footer.html');
+    });
