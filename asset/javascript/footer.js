103725e48a92e6b168c1f5d90266854ad1c88ab5
B0nifaci0 <al221411699@gmail.com>
2022-07-11 21:57:29 -0500

    base_url

--- asset/javascript/footer.js
+++ asset/javascript/footer.js
@@ -1,5 +1,5 @@
 /* para ejecutar jquery o javascript en html es con la etiqueta scrip */
     $(document).ready(function(){
-      $('#menuContainer').load('./layouts/menu.html');
-      $('#footerContainer').load('./layouts/footer.html');
+      $('#menuContainer').load('http://localhost/Aplicacionesweb2022/layouts/menu.html');
+      $('#footerContainer').load('http://localhost/Aplicacionesweb2022/layouts/footer.html');
     });
