79114c9803d769e50936893bd95f9886f126452b
B0nifaci0 <itic.bonifacio@gmail.com>
2022-07-12 19:05:05 -0500

    menu con base url done

--- layouts/menu.php
+++ layouts/menu.php
@@ -1,3 +1,6 @@
+<?php
+include ('../Config.php');
+
 <nav>
    <input type="checkbox" id="check">
    <label for="check" class="checkbtn">
@@ -8,10 +11,11 @@
    </a>
    <ul>
        <li><a class="active" href="#">Inicio</a></li>
-       <li><a href="#">Categorias</a></li>
-       <li><a href="#">Diplomas</a></li>
-       <li><a href="#">Certificados</a></li>
+       <li><a href="<?=$h->get['base_url'].'/views/categories.php'?>">Categorias</a></li>
+       <li><a href="<?=$h->get['base_url'].'/views/diplomas.php'?>">Diplomas</a></li>
+       <li><a href="<?=$h->get['base_url'].'/views/reconocimientos.php'?>">Reconocimientos</a></li>
        <li><a href="./views/auth/login.php">Iniciar</a></li>
        <li><a href="./views/auth/register.php">Registrarte</a></li>
    </ul>
 </nav>
+?>
